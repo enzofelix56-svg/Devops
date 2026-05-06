bf454afefb07c90caf065a5f0131bd195b334d51
Enzo <enzo.souza@venhapranuvem.com.br>
2026-05-06 15:03:41 -0300

    Ajuste no uso do tfstate via storage account

--- Terraform/Infrastructure/main.tf
+++ Terraform/Infrastructure/main.tf
@@ -1,12 +1,3 @@
-#terraform {
-# backend "azurerm" {
-#  resource_group_name  = "rg-dev-aks-001"
-# storage_account_name = "tfstatedevopsproject"
-#container_name       = "tfstate"
-#key                  = "dev.terraform.tfstate"
-#}
-#}
-
 terraform {
   backend "azurerm" {}
 }
